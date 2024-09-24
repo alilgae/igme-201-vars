4c8a82b35c7fe87cf4343cceffcd5dcfae6810d5
KaidenT <89858394+alilgae@users.noreply.github.com>
2024-09-24 09:20:59 -0400

    Created Project

---
+++ BuildingBlocks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Metadata;
+
+namespace BuildingBlocks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int newInt = 17;
+
+            int anotherInt = newInt + 12;
+
+
+            String print = "Hello, ";
+            print += "IGME 201!";
+
+            print += " My two numbers are: " + newInt + ", and " + anotherInt;
+
+            Console.WriteLine(print);
+
+
+        }
+    }
+}
